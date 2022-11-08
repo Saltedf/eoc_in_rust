5937b4384e5d1b9b5bda3e00587921564944450b
salted <saltedf@qq.com>
2022-11-08 13:43:45 +0800

    implemented the definitional interpreter for Lvar

--- src/ast.rs
+++ src/ast.rs
@@ -1,25 +1,29 @@
 use std::collections::HashMap;
 
-#[derive(Debug,Clone,PartialEq, Eq)]
+pub type Env = HashMap<String,i64 >;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Program {
-
-    pub    info : HashMap<String,String>,
-    pub    body : Exp, 
-
+    pub info: HashMap<String, String>,
+    pub env: Env, 
+    pub body: Exp,
 }
 
-#[derive(Debug,Clone,PartialEq, Eq)]
-pub enum PrimType{
-
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum PrimType {
     Read,
     Add,
     Sub,
     Neg,
 }
 
-
-#[derive(Debug,Clone,PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Exp {
     Int(i64),
-    Prim{ op: PrimType, args: Vec<Exp> },
+    Prim { op: PrimType, args: Vec<Exp> },
+    Var(String),
+    Let {var:String,binding: Box<Exp>,body:Box<Exp>},
 }
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,12 @@
-lalrpop_mod!(l_int);
+lalrpop_mod!(l_var);
 mod ast;
+mod interp;
 
 use std::fs::read_to_string;
 use lalrpop_util::lalrpop_mod;
-use ast::*;
 
-fn interp_exp(e : &ast::Exp) -> std::io::Result<i64> {
-    
-    let res =   match e {
-        Exp::Int(n) => n.clone(),
-        Exp::Prim { op:PrimType::Read, .. } =>{
-            let mut buf = String::new();
-            std::io::stdin().read_line(&mut buf)?;
-            buf.pop();// 移除末尾的'\n'
-            buf.parse::<i64>().expect("read expected an integer.")
-        },
-        Exp ::Prim { op:PrimType::Neg, args } => {
-            let e = args.get(0).unwrap();
-            0 - interp_exp(e)?
-        },
-        Exp ::Prim { op:PrimType::Add, args } => {
-            let e1 = interp_exp( args.get(0).unwrap())?;
-            let e2 = interp_exp( args.get(1).unwrap())?;
-            e1 + e2
-        },
-        Exp ::Prim { op:PrimType::Sub, args } => {
-            let e1 = interp_exp( args.get(0).unwrap())?;
-            let e2 = interp_exp( args.get(1).unwrap())?;
-            e1 - e2
-        },
-    };
-    
-    Ok(res)
-}
+use crate::interp::{InterpLvar, Interp};
 
-fn interp_lint(p: &ast::Program) -> std::io::Result<()> {
-    let info  = &p.info;
-    let body = &p.body;
-    
-    let res =  interp_exp(body)?;
-    println!(">> {}",res);
-    
-    Ok(())
-}
 
 
 
@@ -54,11 +18,12 @@ fn main() -> std::io::Result<()>{
     let inputfile = args.next().unwrap();
     let input = read_to_string(inputfile)?;
 
-
-    let ast = l_int::LintParser::new().parse(&input).unwrap();
+    let ast = l_var::LvarParser::new().parse(&input).unwrap();
     println!("{:#?}", ast);
+    println!("======================================");
     
-    interp_lint(&ast)?;
+    let res  =  InterpLvar::interp_program(&ast)?;
+    println!(">> {}", res);
 
     Ok(())
 }
